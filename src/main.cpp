f7734d66ea9ee6afef2985d2419d14f160d833a1
kTrzcinskii <kt.trzcinski@gmail.com>
2025-01-13 14:34:27 +0100

    Add spdlog

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,15 @@
 // Created by Kacper Trzciński on 13.01.2025.
 //
 
+#include <spdlog/spdlog.h>
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "starting\n";
+    spdlog::set_level(spdlog::level::info);
+    spdlog::info("Hello World!");
+    std::cout << "Made it past spdlog\n" << std::endl;
+
     return 0;
 }
