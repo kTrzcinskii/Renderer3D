e9654837b0fe224cf354c3d9f2a2e687cd4f677c
kTrzcinskii <kt.trzcinski@gmail.com>
2025-01-13 13:44:13 +0100

    Add camera class

---
+++ src/camera.cpp
@@ -0,0 +1,132 @@
+//
+// Created by Kacper Trzciński on 13.01.2025.
+//
+
+#include "camera.h"
+
+namespace Renderer3D {
+    Camera::Camera(float screenWidth, float screenHeight, glm::vec3 position, glm::vec3 up, float near, float far, float yaw, float pitch)
+    {
+        _screenWidth = screenWidth;
+        _screenHeight = screenHeight;
+        _front = glm::vec3(0.0f, 0.0f, -1.0f);
+        _movementSpeed = Camera::DEFAULT_SPEED;
+        _mouseSensitivity = Camera::DEFAULT_SENSITIVITY;
+        _zoom = Camera::DEFAULT_ZOOM;
+        _position = position;
+        _worldUp = up;
+        _near = near;
+        _far = far;
+        _yaw = yaw;
+        _pitch = pitch;
+        UpdateCameraVectors();
+    }
+
+    Camera::Camera(float screenWidth, float screenHeight, float posX, float posY, float posZ, float upX, float upY, float upZ, float near, float far, float yaw, float pitch)
+    {
+        _screenWidth = screenWidth;
+        _screenHeight = screenHeight;
+        _front = glm::vec3(0.0f, 0.0f, -1.0f);
+        _movementSpeed = Camera::DEFAULT_SPEED;
+        _mouseSensitivity = Camera::DEFAULT_SENSITIVITY;
+        _zoom = Camera::DEFAULT_ZOOM;
+        _position = glm::vec3(posX, posY, posZ);
+        _worldUp = glm::vec3(upX, upY, upZ);
+        _near = near;
+        _far = far;
+        _yaw = yaw;
+        _pitch = pitch;
+        UpdateCameraVectors();
+    }
+
+    glm::vec3 Camera::GetPosition() const
+    {
+        return _position;
+    }
+
+    glm::mat4 Camera::GetViewMatrix() const
+    {
+        return glm::lookAt(_position, _position + _front, _up);
+    }
+
+    glm::mat4 Camera::GetProjectionMatrix() const
+    {
+        return glm::perspective(glm::radians(_zoom), _screenWidth / _screenHeight, _near, _far);
+    }
+
+    void Camera::Move(const CameraMovementDirection direction, const float deltaTime)
+    {
+        const auto velocity = _movementSpeed * deltaTime;
+        switch (direction)
+        {
+        case CameraMovementDirection::FORWARD:
+            _position += _front * velocity;
+            break;
+        case CameraMovementDirection::BACKWARD:
+            _position -= _front * velocity;
+            break;
+        case CameraMovementDirection::LEFT:
+            _position -= _right * velocity;
+            break;
+        case CameraMovementDirection::RIGHT:
+            _position += _right * velocity;
+            break;
+        case CameraMovementDirection::UP:
+            _position += _up * velocity;
+            break;
+        case CameraMovementDirection::DOWN:
+            _position -= _up * velocity;
+            break;
+        }
+    }
+
+    void Camera::Rotate(const RotateDiff diff, const bool constraintPitch)
+    {
+        _yaw += diff.xOffset * _mouseSensitivity;
+        _pitch += diff.yOffset * _mouseSensitivity;
+
+        if (constraintPitch)
+        {
+            if (_pitch > 89.0f)
+            {
+                _pitch = 89.0f;
+            }
+            else if (_pitch < -89.0f)
+            {
+                _pitch = -89.0f;
+            }
+        }
+
+        UpdateCameraVectors();
+    }
+
+    void Camera::Zoom(const float offset)
+    {
+        _zoom -= offset;
+        if (_zoom < 1.0f)
+        {
+            _zoom = 1.0f;
+        }
+        else if (_zoom > 45.0f)
+        {
+            _zoom = 45.0f;
+        }
+    }
+
+    void Camera::UpdateScreenSize(const float screenWidth, const float screenHeight)
+    {
+        _screenWidth = screenWidth;
+        _screenHeight = screenHeight;
+    }
+
+    void Camera::UpdateCameraVectors()
+    {
+        glm::vec3 front;
+        front.x = static_cast<float>(cos(glm::radians(_yaw)) * static_cast<float>(cos(glm::radians(_pitch))));
+        front.y = static_cast<float>(sin(glm::radians(_pitch)));
+        front.z = static_cast<float>(sin(glm::radians(_yaw))) * static_cast<float>(cos(glm::radians(_pitch)));
+        _front = glm::normalize(front);
+        _right = glm::normalize(glm::cross(_front, _worldUp));
+        _up = glm::normalize(glm::cross(_right, _front));
+    }
+} // Renderer3D
---
+++ src/camera.h
@@ -0,0 +1,75 @@
+//
+// Created by Kacper Trzciński on 13.01.2025.
+//
+
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+namespace Renderer3D {
+
+    enum class CameraMovementDirection
+    {
+        FORWARD,
+        BACKWARD,
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN
+    };
+
+    struct RotateDiff
+    {
+        float xOffset;
+        float yOffset;
+    };
+
+    // TODO: add switching to orthogonal projection
+    class Camera {
+    public:
+        Camera(float screenWidth, float screenHeight, glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float near = Camera::DEFAULT_NEAR, float far = Camera::DEFAULT_FAR, float yaw = Camera::DEFAULT_YAW, float pitch = Camera::DEFAULT_PITCH);
+        Camera(float screenWidth, float screenHeight, float posX, float posY, float posZ, float upX, float upY, float upZ, float near, float far, float yaw, float pitch);
+        // Getters
+        [[nodiscard]] glm::vec3 GetPosition() const;
+        [[nodiscard]] glm::mat4 GetViewMatrix() const;
+        [[nodiscard]] glm::mat4 GetProjectionMatrix() const;
+        // Actions
+        void Move(CameraMovementDirection direction, float deltaTime);
+        void Rotate(RotateDiff diff, bool constraintPitch = true);
+        void Zoom(float offset);
+        void UpdateScreenSize(float screenWidth, float screenHeight);
+    private:
+        // Camera attributes
+        glm::vec3 _position{};
+        glm::vec3 _front{};
+        glm::vec3 _up{};
+        glm::vec3 _right{};
+        glm::vec3 _worldUp{};
+        float _screenWidth;
+        float _screenHeight;
+        float _near;
+        float _far;
+        // Euler angles
+        float _yaw;
+        float _pitch;
+        // Camera options
+        float _movementSpeed;
+        float _mouseSensitivity;
+        float _zoom;
+        // Default values
+        static constexpr float DEFAULT_NEAR = 0.1f;
+        static constexpr float DEFAULT_FAR = 100.0f;
+        static constexpr float DEFAULT_YAW = -90.0f;
+        static constexpr float DEFAULT_PITCH = 0.0f;
+        static constexpr float DEFAULT_SPEED = 2.5f;
+        static constexpr float DEFAULT_SENSITIVITY = 0.1f;
+        static constexpr float DEFAULT_ZOOM = 45.0f;
+
+        void UpdateCameraVectors();
+    };
+
+} // Renderer3D
+
+#endif //CAMERA_H
