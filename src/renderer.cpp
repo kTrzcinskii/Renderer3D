c06c84176b5a2bf0bd5fdd95bcc7c81f4e0985e2
kTrzcinskii <kt.trzcinski@gmail.com>
2025-01-15 18:05:41 +0100

    Create basic rendering pipeline example

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -5,27 +5,58 @@
 #include <spdlog/spdlog.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stb_image/stb_image.h>
 
 #include "renderer.h"
+#include "shader.h"
+#include "model.h"
 
 namespace Renderer3D {
-    Renderer::Renderer()
+    Renderer::Renderer() : _window(Renderer::INITIAL_WIDTH, Renderer::INITIAL_HEIGHT), _freeMovingCamera(Renderer::INITIAL_WIDTH, Renderer::INITIAL_HEIGHT)
     {
+        stbi_set_flip_vertically_on_load(true);
         // We store pointer to renderer inside window so we could easily setup callbacks
         _window.SetUserPointer(this);
         _window.SetWindowResizeCallback(ResizeCallback);
+        _window.SetCursorPositionCallback(CursorPosCallback);
     }
 
     void Renderer::Render()
     {
+        glEnable(GL_DEPTH_TEST);
+
+        // TODO: remove, only for testing
+        _window.LockCursor();
+        const Shader modelShader("../assets/shaders/model_without_light_vertex.glsl", "../assets/shaders/model_without_light_fragment.glsl");
+        const Model model("../assets/models/backpack/backpack.obj");
+
         while (!_window.ShouldClose())
         {
+            // Delta time
+            const auto currentTime = static_cast<float>(glfwGetTime());
+            _deltaTime = currentTime - _lastFrameTime;
+            _lastFrameTime = currentTime;
+
             // Handle input
             ProcessInput();
 
             // Render
             glClearColor(0.2, 0.5f, 0.3f, 0.4f);
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            // Camera matrices
+            const auto projection = _freeMovingCamera.GetProjectionMatrix();
+            const auto view = _freeMovingCamera.GetViewMatrix();
+
+            // Render backpack
+            auto backpackModelMatrix = glm::mat4(1.0f);
+            backpackModelMatrix = glm::translate(backpackModelMatrix, glm::vec3(-0.0f, -1.0f, -3.0f));
+            backpackModelMatrix = glm::scale(backpackModelMatrix, glm::vec3(0.5f, 0.5f, 0.5f));
+            modelShader.Activate();
+            modelShader.SetUniform("model", backpackModelMatrix);
+            modelShader.SetUniform("view", view);
+            modelShader.SetUniform("projection", projection);
+            model.Draw(modelShader);
 
             // Double buffer and events
             _window.SwapBuffers();
@@ -37,19 +68,60 @@ namespace Renderer3D {
     {
         spdlog::info("Window resized: {}x{}", width, height);
         glViewport(0, 0, width, height);
+        _freeMovingCamera.UpdateScreenSize(static_cast<float>(width), static_cast<float>(height));
     }
 
     void Renderer::ProcessInput()
     {
         if (_window.IsKeyPressed(GLFW_KEY_ESCAPE))
         {
+            spdlog::info("Key 'ESC' pressed!");
             _window.Close();
         }
 
         if (_window.IsKeyPressed(GLFW_KEY_W))
         {
             spdlog::info("Key 'W' pressed!");
+            _freeMovingCamera.Move(CameraMovementDirection::FORWARD, _deltaTime);
         }
+
+        if (_window.IsKeyPressed(GLFW_KEY_S))
+        {
+            spdlog::info("Key 'S' pressed!");
+            _freeMovingCamera.Move(CameraMovementDirection::BACKWARD, _deltaTime);
+        }
+
+        if (_window.IsKeyPressed(GLFW_KEY_A))
+        {
+            spdlog::info("Key 'A' pressed!");
+            _freeMovingCamera.Move(CameraMovementDirection::LEFT, _deltaTime);
+        }
+
+        if (_window.IsKeyPressed(GLFW_KEY_D))
+        {
+            spdlog::info("Key 'D' pressed!");
+            _freeMovingCamera.Move(CameraMovementDirection::RIGHT, _deltaTime);
+        }
+    }
+
+    void Renderer::ProcessMouseMovement(double xPos, double yPos)
+    {
+        spdlog::info("Mouse moved: {}x{}", xPos, yPos);
+        const auto x = static_cast<float>(xPos);
+        const auto y = static_cast<float>(yPos);
+        if (_firstMouseMove)
+        {
+            _mouseXPos = x;
+            _mouseYPos = y;
+            _firstMouseMove = false;
+        }
+
+        const auto xOffset = x - _mouseXPos;
+        const auto yOffset = _mouseYPos - y;
+        _mouseXPos = x;
+        _mouseYPos = y;
+
+        _freeMovingCamera.Rotate({xOffset, yOffset});
     }
 
     void Renderer::ResizeCallback(GLFWwindow* window, const int width, const int height)
@@ -64,4 +136,15 @@ namespace Renderer3D {
         renderer->ProcessWindowResize(width, height);
     }
 
+    void Renderer::CursorPosCallback(GLFWwindow* window, double xPos, double yPos)
+    {
+        // ReSharper disable once CppTooWideScopeInitStatement
+        const auto renderer = static_cast<Renderer*>(glfwGetWindowUserPointer(window));
+        if (renderer == nullptr)
+        {
+            spdlog::error("Window has user pointer set to NULL");
+            return;
+        }
+        renderer->ProcessMouseMovement(xPos, yPos);
+    }
 } // Renderer3D
--- src/renderer.h
+++ src/renderer.h
@@ -6,6 +6,7 @@
 #define RENDERER_H
 
 #include "window.h"
+#include "camera.h"
 
 namespace Renderer3D {
 
@@ -14,15 +15,29 @@ namespace Renderer3D {
         Renderer();
         void Render();
     private:
-        Window _window;
+        // Renderer data
         float _lastFrameTime = 0.0;
+        float _deltaTime = 0.0;
+        bool _firstMouseMove = true;
+        float _mouseXPos = Renderer::INITIAL_WIDTH / 2.0f;
+        float _mouseYPos = Renderer::INITIAL_HEIGHT / 2.0f;;
+
+        // Objects
+        Window _window;
+        Camera _freeMovingCamera;
 
         // Actions
         void ProcessWindowResize(int width, int height);
         void ProcessInput();
+        void ProcessMouseMovement(double xPos, double yPos);
 
         // Window callbacks
         static void ResizeCallback(GLFWwindow* window, int width, int height);
+        static void CursorPosCallback(GLFWwindow* window, double xPos, double yPos);
+
+        // Consts
+        static constexpr size_t INITIAL_WIDTH = 1600;
+        static constexpr size_t INITIAL_HEIGHT = 800;
     };
 
 } // Renderer3D
