944aa6a490f4d2a9139c7abe840f6db350816388
kTrzcinskii <kt.trzcinski@gmail.com>
2025-01-18 17:40:20 +0100

    Create scene class

---
+++ src/scene.cpp
@@ -0,0 +1,73 @@
+//
+// Created by Kacper Trzciński on 18.01.2025.
+//
+
+#include <utility>
+#include <spdlog/spdlog.h>
+
+#include "scene.h"
+
+namespace Renderer3D {
+    Scene::Scene(std::unordered_map<std::string, Entity> entities, std::unique_ptr<PointLightsContainer> pointLightsContainer) : _entities(std::move(entities)), _pointLightsContainer(std::move(pointLightsContainer))
+    {
+    }
+
+    void Scene::AddEntity(const std::string& name, const Entity& entity)
+    {
+        _entities.emplace(name, entity);
+    }
+
+    void Scene::AddEntityUpdateFunction(const std::string& name, const UpdateEntityFunctionType& function)
+    {
+        // ReSharper disable once CppTooWideScopeInitStatement
+        const auto it = _entities.find(name);
+        if (it == _entities.end())
+        {
+            spdlog::error("Trying to set entity update function for entity that is not in the scene: {}", name);
+            return;
+        }
+        _updateEntityFunctions.emplace(name, function);
+    }
+
+    void Scene::UpdatePointLightContainer(std::unique_ptr<PointLightsContainer> pointLightsContainer)
+    {
+        _pointLightsContainer = std::move(pointLightsContainer);
+    }
+
+    void Scene::UpdateEntities(const float deltaTime)
+    {
+        for (auto& [name, func] : _updateEntityFunctions)
+        {
+            // ReSharper disable once CppTooWideScopeInitStatement
+            const auto it = _entities.find(name);
+            if (it != _entities.end())
+            {
+                func(it->second, deltaTime);
+            }
+            else
+            {
+                spdlog::error("Trying to update entity for entity that is not in the scene: {}", name);
+            }
+        }
+    }
+
+    void Scene::RenderEntitiesToGeometryPass(const std::shared_ptr<Shader>& geometryPassShader, const glm::mat4& view, const glm::mat4& projection) const
+    {
+        geometryPassShader->SetUniform("view", view);
+        geometryPassShader->SetUniform("projection", projection);
+        for (const auto& [_, entity] : _entities)
+        {
+           entity.Draw(geometryPassShader);
+        }
+    }
+
+    void Scene::SetLightingPassShaderData(const std::shared_ptr<Shader>& lightingPassShader) const
+    {
+        _pointLightsContainer->SetLightingPassPointLightsData(lightingPassShader);
+    }
+
+    void Scene::RenderPointLightsForwardRendering(const glm::mat4& view, const glm::mat4& projection) const
+    {
+        _pointLightsContainer->RenderPointLights(view, projection);
+    }
+} // Renderer3D
---
+++ src/scene.h
@@ -0,0 +1,35 @@
+//
+// Created by Kacper Trzciński on 18.01.2025.
+//
+
+#ifndef SCENE_H
+#define SCENE_H
+
+#include <glad/glad.h>
+
+#include "shader.h"
+#include "entity.h"
+#include "point_lights_container.h"
+
+namespace Renderer3D {
+    using UpdateEntityFunctionType = std::function<void(Entity&,float)>;
+
+    class Scene {
+    public:
+        explicit Scene(std::unordered_map<std::string, Entity> entities = std::unordered_map<std::string, Entity>(), std::unique_ptr<PointLightsContainer> point_lights_container = std::make_unique<PointLightsContainer>());
+        void AddEntity(const std::string& name, const Entity& entity);
+        void AddEntityUpdateFunction(const std::string& name, const UpdateEntityFunctionType& function);
+        void UpdatePointLightContainer(std::unique_ptr<PointLightsContainer> pointLightsContainer);
+        void UpdateEntities(float deltaTime);
+        void RenderEntitiesToGeometryPass(const std::shared_ptr<Shader>& geometryPassShader, const glm::mat4& view, const glm::mat4& projection) const;
+        void SetLightingPassShaderData(const std::shared_ptr<Shader>& lightingPassShader) const;
+        void RenderPointLightsForwardRendering(const glm::mat4& view, const glm::mat4& projection) const;
+    private:
+        std::unordered_map<std::string, Entity> _entities;
+        std::unordered_map<std::string, UpdateEntityFunctionType> _updateEntityFunctions;
+        std::unique_ptr<PointLightsContainer> _pointLightsContainer;
+    };
+
+} // Renderer3D
+
+#endif //SCENE_H
